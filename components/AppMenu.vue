7238a59a1cd2122da8d393c77fbbf9e321c3f3a1
Gemma <gemma.stephen@visitscotland.com>
2022-04-28 14:58:50 +0100

    Adding a cart

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -2,16 +2,34 @@
   <nav>
     <ul>
       <li>
-        <nuxt-link exact to="/">Home</nuxt-link>
+        <nuxt-link exact to="/">
+          <AppLogo />
+        </nuxt-link>
       </li>
       <li>
-        <nuxt-link exact to="restaurants">Restaurants</nuxt-link>
+        <nuxt-link to="/restaurants">Restaurants</nuxt-link>
       </li>
     </ul>
+    <nuxt-link to="/cart">Cart</nuxt-link>
+    
+    <div class="smallnum" v-if="totalItems > 0">
+        {{totalItems}}
+    </div>
   </nav>
 </template>
 
 <script>
+import AppLogo from "@/components/AppLogo.vue";
+import { mapGetters } from 'vuex'
+
+export default {
+  components: {
+    AppLogo,
+  },
+  computed: {
+      ...mapGetters(["totalItems"])
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
